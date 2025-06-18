1f1ad8fb3c49120d22dcae4cfd96f95150fe19ec
ModsonTech45 <tandemodson55@gmail.com>
2025-06-18 02:03:06 +0000

    Make the database live

--- db.js
+++ db.js
@@ -1,13 +1,31 @@
 
+require('dotenv').config();
 
 const { Pool } = require('pg');
 
+
+const neonConnectionString = process.env.DATABASE_URL;
+
+
+if (!neonConnectionString) {
+    console.error('DATABASE_URL environment variable is not set.');
+    console.error('Please make sure you have a .env file with DATABASE_URL="your_neon_connection_string".');
+    process.exit(1);
+}
+
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'Attendance',
-  password: '1234',
-  port: 5432,
+  connectionString: neonConnectionString,
+
+  ssl: {
+    rejectUnauthorized: true
+  }
+});
+
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+
+  process.exit(-1);
 });
 
 module.exports = pool;
