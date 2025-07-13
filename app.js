0f9e346b0f0530c7e2d3893a55f3e52b8dcd1ff4
ModsonTech45 <tandemodson55@gmail.com>
2025-07-13 16:07:27 +0000

    cors issue

--- app.js
+++ app.js
@@ -5,25 +5,30 @@ const cors = require('cors');
 
 const adminRoutes = require('./users-routes/admin');
 const teacherRoutes = require('./users-routes/teacher');
-const resetPasswordRoutes = require('./users-routes/reset-password');
-const deviceRoutes = require('./devices/registerdevice');
+const resetPasswordRoutes = require('./users-routes/reset-password')
+const deviceRoutes = require('./devices/registerdevice'); // adjust path as needed
 const categoryRoutes = require('./category/categories');
+
+
+
 
 const app = express();
 const port = 3000;
 
-// Define CORS options
-const corsOptions = {
+
+
+
+const allowedOrigins = [
+  'http://localhost:8080',
+  'http://127.0.0.1:5500',
+   'http://localhost:5173',
+  'https://rfid-attendance-synctuario-theta.vercel.app',
+  'https://rfid-attendancesystem-backend-project.onrender.com'
+];
+
+app.use(cors({
   origin: function (origin, callback) {
-    const allowedOrigins = [
-      'http://localhost:8080',
-      'http://127.0.0.1:5500',
-      'http://localhost:5173',
-      'https://localhost:5173',
-      'https://rfid-attendance-synctuario-theta.vercel.app',
-      'https://rfid-attendancesystem-backend-project.onrender.com'
-    ];
-    if (!origin) return callback(null, true);
+    if (!origin) return callback(null, true); // Allow non-browser requests
     if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
@@ -32,19 +37,23 @@ const corsOptions = {
   },
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   credentials: true,
-};
+}));
 
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // 👈 handles preflight
+
+
+
 
 app.use(bodyParser.json());
 
-app.use('/api/categories', categoryRoutes);
+app.use('/api/categories', categoryRoutes); // ✅ REGISTER the route here
+
 app.use('/api/devices', deviceRoutes);
 app.use('/api/admins', adminRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/teachers', teacherRoutes);
 app.use('/api/reset', resetPasswordRoutes);
+
+
 app.use('/api/students', require('./student-routes/students'));
 app.use('/api/attendance', require('./student-routes/attendance'));
 app.use('/api/scan', require('./student-routes/scan'));
