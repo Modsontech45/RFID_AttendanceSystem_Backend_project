1f466c2f7925bc1d1afdcb75e6916a34aee85eb6
Modson Tande <131668620+Modsontech45@users.noreply.github.com>
2025-07-21 22:28:15 +0000

    subscription

--- student-routes/attendance.js
+++ student-routes/attendance.js
@@ -1,36 +1,33 @@
-const express = require('express');
-const pool = require('../db');
+const express = require("express");
+const pool = require("../db");
 const router = express.Router();
-const verifyApiKey = require('../middleware/verifyApiKey');
-const getMessage = require('../utils/messages');
-const { checkSubscription } = require('../middleware/auth');
+const verifyApiKey = require("../middleware/verifyApiKey");
+const getMessage = require("../utils/messages");
+const { checkSubscription } = require("../middleware/auth");
 // GET attendance records filtered by API key
-router.get('/', verifyApiKey, async (req, res) => {
-  const lang = req.headers["accept-language"]?.toLowerCase().split(",")[0] || "en";
- const admin = req.admin;
+router.get("/", verifyApiKey, async (req, res) => {
+  const lang =
+    req.headers["accept-language"]?.toLowerCase().split(",")[0] || "en";
+  const admin = req.admin;
   try {
     const requesterApiKey = req.user.api_key;
 
     if (!requesterApiKey) {
-      return res.status(403).json({ message: getMessage(lang, "attendance.noApiKey") });
+      return res
+        .status(403)
+        .json({ message: getMessage(lang, "attendance.noApiKey") });
     }
     // Check subscription status
-async function checkSubscription(admin) {
-  if (!admin || !admin.subscription_status || !admin.subscription_expires_at) {
-    console.error("Invalid admin object passed to checkSubscription:", admin);
-    return "expired"; // or throw error, based on logic
-  }
+    const subStatus = "expired" //await checkSubscription(admin);
 
-  const currentDate = new Date();
-  const expiryDate = new Date(admin.subscription_expires_at);
-
-  if (admin.subscription_status === "active" && expiryDate > currentDate) {
-    return "active";
-  } else {
-    return "expired";
-  }
-}
-
+    if (subStatus === "expired") {
+      return res.status(403).json({
+        message: "Subscription expired. Please renew.",
+        redirectTo:
+          "https://rfid-attendance-synctuario-theta.vercel.app/pricing",
+        subscriptionExpired: true,
+      });
+    }
 
     const attendances = await pool.query(
       `SELECT * FROM attendance WHERE api_key = $1`,
@@ -38,7 +35,9 @@ async function checkSubscription(admin) {
     );
 
     if (attendances.rows.length === 0) {
-      return res.status(403).json({ message: getMessage(lang, "attendance.noneForKey") });
+      return res
+        .status(403)
+        .json({ message: getMessage(lang, "attendance.noneForKey") });
     }
 
     res.json(attendances.rows);
