6084ef3a8eff823c7f38df79ec35b2153b10a0bc
Modson Tande <131668620+Modsontech45@users.noreply.github.com>
2025-09-16 13:50:30 +0000

    fix option issues

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 }));
-app.options("*", cors());
+// app.options("*", cors());
 app.use(bodyParser.json());
 
 app.use("/api/categories", categoryRoutes); // ✅ REGISTER the route here
